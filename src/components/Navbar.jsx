26c8034a72b659f7b15d8d38edef183f2bc18c51
Saurabh Kumar <saurabhkumarxmpt@gmail.com>
2025-07-11 22:27:57 +0530

    make the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,35 @@
-const Navbar=()=>{
-    return(
-        <>
-        <h1>this is navbar</h1>
-        </>
-    )
-}
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="px-6 py-4">
+      <div className="max-w-7xl mx-auto flex items-center justify-between">
+        {/* Centered menu using flex-grow */}
+        <ul className="flex flex-grow justify-center space-x-6 text-lg font-medium">
+          <li>
+            <Link to="/" className="hover:text-blue-600">Home</Link>
+          </li>
+          <li>
+            <Link to="/about" className="hover:text-blue-600">About</Link>
+          </li>
+          <li>
+            <Link to="/resume" className="hover:text-blue-600">Resume</Link>
+          </li>
+          <li>
+            <Link to="/project" className="hover:text-blue-600">Project</Link>
+          </li>
+          <li>
+            <Link to="/contact" className="hover:text-blue-600">Contact</Link>
+          </li>
+        </ul>
+
+        {/* Right side mobile number */}
+        {/* <div className="text-sm font-semibold">
+          📞 +91 9876543210
+        </div> */}
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,14 @@
 import Navbar from "../components/Navbar";
 
-const HomePage=()=>{
-    return(
-        <>
-        <Navbar/>
-        </>
-    )
-}
+const HomePage = () => {
+  return (
+    <>
+    <div className="bg-[url('./home-page-image.jpg')] min-h-screen bg-cover text-white">
+    <Navbar/>
+    <h1>saurabh kumar</h1>
+    </div>
+    </>
+  );
+};
 
 export default HomePage;
